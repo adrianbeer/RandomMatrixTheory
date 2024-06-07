d3ec74257a55ae12ea3e4fd7c61bb497e9f3d2f8
adrianbeer <adrianbeer@fastmail.com>
2024-06-07 13:28:49 +0200

    Adds exercise about estimating spectrum of random matrix

--- deterministic_matrices.py
+++ deterministic_matrices.py
@@ -1,43 +1,21 @@
+
 import numpy as np
-from numpy.linalg import svd, eig, inv, eigvals
+from numpy.linalg import eigh, inv, eigvals, eigh, eig, norm
 import matplotlib.pyplot as plt
 from tqdm import tqdm
-
-N = 100
-N_samples = 100
-sigmas = (0.1, 0.2, 0.3)
+from fitter import Fitter
 
 
-sigma = sigmas[0]
+def generate_GOE(N=100, sigma=1):
+    # Generates Gaussian Orthogonal Ensemble. rotationally invariant with diagonal elements have twice the variance as off-diagonal elements
+    H = np.random.normal(loc=0, scale=np.sqrt(sigma**2/(2*N)), size=N*N).reshape((N, N))
+    goe = (H + H.T)
+    return goe
 
-# M is a real symmetric orthogonal matrix with M = M.T = M^-1
-def generate_M():
-    H = np.random.normal(loc=0, scale=1, size=N*N).reshape((N, N))
-    M = sigma*(H + H.T) / np.sqrt(2*N)
-    D, P = eig(M)
-    D = np.sign(D)
-    M = P * D * inv(P)
-    return M
 
 # Wigner Matrix NxN, i.e. real symmetric matrix with IID Gaussian random numbers with 
 # zero mean and variance sigma^2/N 
-def generate_X(sigma):
+def generate_Wigner(N, sigma):
     H = np.random.normal(loc=0, scale=1, size=N*N).reshape((N, N))
     X = sigma*(H + H.T) / np.sqrt(2*N)
     return X
-
-fig, ax = plt.subplots(nrows=len(sigmas), ncols=2)
-for sigma_idx, sigma in enumerate(sigmas):
-    M = generate_M()
-    eigs_list = []
-    for i in tqdm(range(N_samples)):
-        X = generate_X(sigma)
-        E = M + X
-        eigs = eigvals(M)
-        if i == 0:
-            ax[0, sigma_idx].hist(eigs, density=True, bins=30)
-        eigs_list.append(eigs)
-
-        # Plot Eigenvalues of E 
-        ax[1, sigma_idx].hist(eigs_list, density=True, bins=30)
-fig.savefig("plot1.pdf")
